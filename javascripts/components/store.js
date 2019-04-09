de034e77a63ead14954bacb0c280fbaec303a19a
Emily DeWitt <emilykdewitt@gmail.com>
2019-04-09 18:54:40 -0500

    Created button click function for add to cart to console log

--- javascripts/components/store.js
+++ javascripts/components/store.js
@@ -2,13 +2,20 @@
 import book from '../helpers/book.js';
 import util from '../helpers/util.js';
 
+const addToCartEvent = (e) => {
+    e.preventDefault();
+    const bookInfo = book.getBook();
+    console.log('book:', bookInfo);
+}
+
 const makeStore = () => {
     const bookInfo = book.getBook();
     let domString = '<h2>Our Only Book</h2>';
     domString += `<h3>${bookInfo.price}</h3>`;
-    domString += `<img src="${bookInfo.image}" alt="book cover">`;
-    domString += `<button class="btn btn-danger">Add to Cart</button>`;
+    domString += `<img src="${bookInfo.image}" alt="book cover" class="book-cover">`;
+    domString += `<button id="cartBtn" class="btn btn-danger">Add to Cart</button>`;
     util.printToDom('store-container', domString);
+    document.getElementById('cartBtn').addEventListener('click', addToCartEvent);
 };
 
 export default { makeStore };
